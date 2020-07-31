b9ec0ab6bb004b5aa82b491ec107c2096a3e93ff
sdoerl <sebastian.doerl@ottogroup.com>
2020-07-31 13:12:22 +0200

    Use two leds in hi and lo to indicate layer, third led is for caps

--- keyboards/ergodox_ez/keymaps/s3dse/keymap.c
+++ keyboards/ergodox_ez/keymaps/s3dse/keymap.c
@@ -1,21 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
 #include "keymap_german.h"
-// #include "keymap_nordic.h"
-// #include "keymap_french.h"
-// #include "keymap_spanish.h"
-// #include "keymap_hungarian.h"
-// #include "keymap_swedish.h"
-// #include "keymap_br_abnt2.h"
-// #include "keymap_canadian_multilingual.h"
-// #include "keymap_german_ch.h"
-// #include "keymap_jp.h"
-// #include "keymap_bepo.h"
-// #include "keymap_italian.h"
-// #include "keymap_slovenian.h"
-// #include "keymap_danish.h"
-// #include "keymap_norwegian.h"
-// #include "keymap_portuguese.h"
 
 #define KC_MAC_UNDO LGUI(KC_Z)
 #define KC_MAC_CUT LGUI(KC_X)
@@ -236,13 +221,10 @@ bool led_update_user(led_t led_state) {
     static uint8_t caps_state = 0;
     if (caps_state != led_state.caps_lock) {
         if (led_state.caps_lock) {
+          ergodox_right_led_1_set(LED_BRIGHTNESS_LO);
           ergodox_right_led_1_on();
-          ergodox_right_led_2_on();
-          ergodox_right_led_3_on();
         } else {
           ergodox_right_led_1_off();
-          ergodox_right_led_2_off();
-          ergodox_right_led_3_off();
         }
         caps_state = led_state.caps_lock;
     }
@@ -254,34 +236,41 @@ uint32_t layer_state_set_user(uint32_t state) {
   uint8_t layer = biton32(state);
 
   ergodox_board_led_off();
-  ergodox_right_led_1_off();
   ergodox_right_led_2_off();
   ergodox_right_led_3_off();
   switch (layer) {
     case 1:
-      ergodox_right_led_1_on();
+      ergodox_right_led_2_set(LED_BRIGHTNESS_LO);
+      ergodox_right_led_2_on();
       break;
     case 2:
+      ergodox_right_led_2_set(LED_BRIGHTNESS_HI);
       ergodox_right_led_2_on();
       break;
     case 3:
+      ergodox_right_led_3_set(LED_BRIGHTNESS_LO);
       ergodox_right_led_3_on();
       break;
     case 4:
-      ergodox_right_led_1_on();
-      ergodox_right_led_2_on();
+      ergodox_right_led_3_set(LED_BRIGHTNESS_HI);
+      ergodox_right_led_3_on();
       break;
     case 5:
-      ergodox_right_led_1_on();
+      ergodox_right_led_2_set(LED_BRIGHTNESS_LO);
+      ergodox_right_led_2_on();
+      ergodox_right_led_3_set(LED_BRIGHTNESS_LO);
       ergodox_right_led_3_on();
       break;
     case 6:
+      ergodox_right_led_2_set(LED_BRIGHTNESS_HI);
       ergodox_right_led_2_on();
+      ergodox_right_led_3_set(LED_BRIGHTNESS_LO);
       ergodox_right_led_3_on();
       break;
     case 7:
-      ergodox_right_led_1_on();
+      ergodox_right_led_2_set(LED_BRIGHTNESS_HI);
       ergodox_right_led_2_on();
+      ergodox_right_led_3_set(LED_BRIGHTNESS_HI);
       ergodox_right_led_3_on();
       break;
     default:
