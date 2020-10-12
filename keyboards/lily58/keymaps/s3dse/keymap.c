7ec1638bad88badaaaa66e5f6558107832f154b8
Sebastian Doerl <sebastian.doerl@ottogroup.com>
2020-10-12 19:52:21 +0200

    copy all macros to lily58 layout

--- keyboards/lily58/keymaps/s3dse/keymap.c
+++ keyboards/lily58/keymaps/s3dse/keymap.c
@@ -13,6 +13,32 @@ extern uint8_t is_master;
 enum custom_keycodes {
   ST_MACRO_0 = SAFE_RANGE,
   ST_MACRO_1,
+  ST_MACRO_2,
+  ST_MACRO_3,
+  ST_MACRO_4,
+  ST_MACRO_5,
+  ST_MACRO_6,
+  ST_MACRO_7,
+  ST_MACRO_8,
+  ST_MACRO_9,
+  ST_MACRO_10,
+  ST_MACRO_11,
+  ST_MACRO_12,
+  ST_MACRO_13,
+  ST_MACRO_14,
+  ST_MACRO_15,
+  ST_MACRO_16,
+  ST_MACRO_17,
+  ST_MACRO_18,
+  ST_MACRO_19,
+  ST_MACRO_20,
+  ST_MACRO_21,
+  ST_MACRO_22,
+  ST_MACRO_23,
+  ST_MACRO_24,
+  ST_MACRO_25,
+  ST_MACRO_26,
+  MCR_UML_A_UP,
 };
 
 enum layer_number {
@@ -21,6 +47,8 @@ enum layer_number {
   _SYMRIGHT,
   _NUMRIGHT,
   _NAV,
+  _UMLAUT,
+  _EDIT,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -41,11 +69,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_WORKMAN] = LAYOUT( \
-  KC_ESC,   KC_1,           KC_2,           KC_3,    KC_4,    KC_5,                                   KC_6,    KC_7,          KC_8,    KC_9,    KC_0,    KC_GRV, \
+  MO(5),   KC_1,           KC_2,           KC_3,    KC_4,    KC_5,                                   KC_6,    KC_7,          KC_8,    KC_9,    KC_0,    MO(5), \
   KC_TAB,   KC_Q,           KC_D,           KC_R,    KC_W,    KC_B,                                   KC_J,    KC_F,          KC_U,    KC_P,    KC_SCOLON,    KC_MINS, \
   KC_LCTRL, LGUI_T(KC_A),   KC_S,           KC_H,    LT(2,KC_T),    LT(3,KC_G),                                   KC_Y,    LT(1,KC_N),    KC_E,    KC_O,    RGUI_T(KC_I), KC_QUOT, \
-  KC_LSFT,  LCTL_T(KC_Z),   LALT_T(KC_X),   KC_M,    KC_C,    KC_V,       KC_BSPACE,          KC_TAB,  KC_K,    KC_L,         KC_COMMA, RALT_T(KC_DOT),  RCTL_T(KC_SLASH),  KC_RSFT, \
-                                            MO(4), KC_DELETE, LSFT_T(KC_SPACE), KC_BSPACE, KC_TAB, RSFT_T(KC_ENTER), KC_ESCAPE, KC_BSPC \
+  KC_LSFT,  LCTL_T(KC_Z),   LALT_T(KC_X),   KC_M,    KC_C,    KC_V,       MO(4),          KC_TAB,  KC_K,    KC_L,         KC_COMMA, RALT_T(KC_DOT),  RCTL_T(KC_SLASH),  KC_RSFT, \
+                                            MO(6), KC_DELETE, LSFT_T(KC_SPACE), KC_BSPACE, KC_TAB, RSFT_T(KC_ENTER), KC_ESCAPE, MO(6) \
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -86,8 +114,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_SYMRIGHT] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                     KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, \
   _______, _______, _______, _______, _______, _______,                        KC_MINUS,    KC_EQUAL,    KC_LABK,    KC_RABK,    KC_ASTR,    _______, \
-  _______, _______, _______, _______, XXXXXXX, _______,                     KC_UNDS, KC_LCBR, KC_RCBR, ST_MACRO_0,   KC_PLUS, _______, \
-  _______, _______, _______, _______, _______, _______,  _______, _______,  KC_AMPR, KC_QUOTE, KC_DQUO,  ST_MACRO_1, KC_BSLASH, _______, \
+  _______, _______, _______, _______, XXXXXXX, _______,                     KC_UNDS, KC_LCBR, KC_RCBR, ST_MACRO_25,   KC_PLUS, _______, \
+  _______, _______, _______, _______, _______, _______,  _______, _______,  KC_AMPR, KC_QUOTE, KC_DQUO,  ST_MACRO_26, KC_BSLASH, _______, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
 ),
 /* ADJUST
@@ -114,10 +142,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NAV] = LAYOUT( \
   _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-  _______, _______, _______, _______, _______, _______,                   _______, KC_7, KC_8, KC_9, KC_COMMA, _______, \
+  _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______, \
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_1, KC_2, KC_3, KC_DOT, _______,\
-                             XXXXXXX, _______, _______, _______, _______,  _______, _______, _______ \
+  _______, _______, _______, _______, _______, _______, XXXXXXX, _______, KC_HOME, ST_MACRO_2, ST_MACRO_3, KC_END, _______, _______,\
+                             _______, _______, _______, _______, _______,  _______, _______, _______ \
+  ),
+
+  [_UMLAUT] = LAYOUT( \
+  XXXXXXX, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, XXXXXXX, \
+  _______, MCR_UML_A_UP, _______, _______, _______, _______,                   _______, _______, ST_MACRO_6, ST_MACRO_7, _______, _______, \
+  _______, ST_MACRO_4, ST_MACRO_5, _______, _______, _______,                   _______, _______, ST_MACRO_8, ST_MACRO_9, _______, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,\
+                             _______, _______, _______, _______, _______,  _______, _______, _______ \
+  ),
+  
+  [_EDIT] = LAYOUT( \
+  _______, _______, _______, _______, _______, _______,                  _______, _______, _______, _______, _______, _______, \
+  _______, _______, _______, ST_MACRO_10, _______, _______,              ST_MACRO_15, ST_MACRO_16, _______, _______, _______, _______, \
+  _______, _______, ST_MACRO_11, ST_MACRO_12, ST_MACRO_13, ST_MACRO_14,  ST_MACRO_17, ST_MACRO_18, ST_MACRO_19, _______, _______, _______, \
+  XXXXXXX, _______, _______, _______, _______, _______, _______, _______, ST_MACRO_20, ST_MACRO_21, ST_MACRO_22, _______, _______, XXXXXXX,\
+                             _______, _______, _______, _______, _______,  _______, _______, _______ \
   )
 };
 
@@ -169,23 +213,175 @@ void oled_task_user(void) {
 static bool shifted = false;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode)
-  {
-  case LSFT_T(KC_SPACE):
-      shifted = record->event.pressed;
-      return true;
-      break;
-    case RSFT_T(KC_ENTER):
-      shifted = record->event.pressed;
-      return true;
-      break;
+  switch (keycode) {
     case ST_MACRO_0:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTL(SS_TAP(X_SPACE)));
+
+    }
+    break;
+    case ST_MACRO_1:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LGUI(SS_LSFT(SS_TAP(X_SPACE))));
+
+    }
+    break;
+    case ST_MACRO_2:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_LEFT)));
+
+    }
+    break;
+    case ST_MACRO_3:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_RIGHT)));
+
+    }
+    break;
+    case ST_MACRO_4:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_U)) SS_DELAY(100) SS_TAP(X_A));
+
+    }
+    break;
+    case MCR_UML_A_UP:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_U)) SS_LSFT(SS_TAP(X_A)));
+    }
+    break;
+    case ST_MACRO_5:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_S)));
+
+    }
+    break;
+    case ST_MACRO_6:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_U)) SS_DELAY(100) SS_LSFT(SS_TAP(X_U)));
+
+    }
+    break;
+    case ST_MACRO_7:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_U)) SS_DELAY(100) SS_LSFT(SS_TAP(X_O)));
+
+    }
+    break;
+    case ST_MACRO_8:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_U)) SS_DELAY(100) SS_TAP(X_U));
+
+    }
+    break;
+    case ST_MACRO_9:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_U)) SS_DELAY(100) SS_TAP(X_O));
+
+    }
+    break;
+    case ST_MACRO_10:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_LGUI(SS_TAP(X_T))));
+
+    }
+    break;
+    case ST_MACRO_11:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_LGUI(SS_LSFT(SS_TAP(X_LEFT)))));
+
+    }
+    break;
+    case ST_MACRO_12:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_LGUI(SS_LSFT(SS_TAP(X_DOWN)))));
+
+    }
+    break;
+    case ST_MACRO_13:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_LGUI(SS_LSFT(SS_TAP(X_UP)))));
+
+    }
+    break;
+    case ST_MACRO_14:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_LGUI(SS_LSFT(SS_TAP(X_RIGHT)))));
+
+    }
+    break;
+    case ST_MACRO_15:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LGUI(SS_TAP(X_F2)));
+
+    }
+    break;
+    case ST_MACRO_16:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LGUI(SS_LSFT(SS_TAP(X_F))));
+
+    }
+    break;
+    case ST_MACRO_17:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_RIGHT))));
+
+    }
+    break;
+    case ST_MACRO_18:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_UP)));
+
+    }
+    break;
+    case ST_MACRO_19:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_LSFT(SS_TAP(X_UP))));
+
+    }
+    break;
+    case ST_MACRO_20:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_LEFT))));
+
+    }
+    break;
+    case ST_MACRO_21:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_TAP(X_DOWN)));
+
+    }
+    break;
+    case ST_MACRO_22:
+    if (record->event.pressed) {
+      SEND_STRING(SS_LALT(SS_LSFT(SS_TAP(X_DOWN))));
+
+    }
+    break;
+    case ST_MACRO_23:
+    if (record->event.pressed) {
+      SEND_STRING("ment");
+    }
+    break;
+    case ST_MACRO_24:
+    if (record->event.pressed) {
+      SEND_STRING("ion");
+    }
+    break;
+    case LSFT_T(KC_SPACE):
+      shifted = record->event.pressed;
+      return true;
+      break;
+    case RSFT_T(KC_SPACE):
+      shifted = record->event.pressed;
+      return true;
+      break;
+    case ST_MACRO_25:
       
       if (record->event.pressed) {
         if (shifted) {
 
           unregister_code16(LSFT_T(KC_SPACE));
-          unregister_code16(RSFT_T(KC_ENTER));
+          unregister_code16(RSFT_T(KC_SPACE));
           SEND_STRING("<-");
         } else {
           SEND_STRING("->");
@@ -193,22 +389,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
       }
     break;
-    case ST_MACRO_1:
+    case ST_MACRO_26:
       if (record->event.pressed) {
         if (shifted) {
           unregister_code16(LSFT_T(KC_SPACE));
-          unregister_code16(RSFT_T(KC_ENTER));
+          unregister_code16(RSFT_T(KC_SPACE));
           SEND_STRING("<=");
-          
         } else {
           SEND_STRING("=>");
         }
       }
     break;
-  
-  default:
-    break;
   }
+
+
   if (record->event.pressed) {
 #ifdef OLED_DRIVER_ENABLE
     set_keylog(keycode, record);
