566c3137d957814f18e93d5b1b781da5a05ca3e8
sdoerl <sebastian.doerl@ottogroup.com>
2021-05-21 20:15:51 +0200

    fix layer toggle to rgb

--- keyboards/crkbd/keymaps/s3dse/keymap.c
+++ keyboards/crkbd/keymaps/s3dse/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   MO(5), KC_Q, KC_D, KC_R, KC_W, LT(4, KC_B),                                       KC_J,    KC_F,        KC_U,      KC_P, KC_SCOLON,     MO(5), \
   KC_LGUI, LGUI_T(KC_A), KC_S, KC_H, LT(2,KC_T), LT(3,KC_G),                     LT(7,KC_Y), LT(1,KC_N),    KC_E,      KC_O, RGUI_T(KC_I),  TG(_QWERTY), \
   MO(6),   LCTL_T(KC_Z),   LALT_T(KC_X),   KC_M,    KC_C, KC_V,         KC_K,    KC_L,       KC_COMMA,  LALT_T(KC_DOT),  RCTL_T(KC_SLASH),   MO(6), \
-                        KC_DELETE, LSFT_T(KC_SPACE), KC_BSPACE,              LT(_FN, KC_TAB),   RSFT_T(KC_ENTER), LT(KC_ESCAPE, _RGB) \
+                        KC_DELETE, LSFT_T(KC_SPACE), KC_BSPACE,              LT(_FN, KC_TAB),   RSFT_T(KC_ENTER), LT(_RGB, KC_ESCAPE) \
                                       //`--------------------------'  `--------------------------'
   ),
   [_SYMLEFT] = LAYOUT( \
